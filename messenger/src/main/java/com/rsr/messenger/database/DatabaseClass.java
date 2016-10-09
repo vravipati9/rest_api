cf656dce8273a3a445e794c87f04e96dd05c7a97
vvsrk <vvsrk.siva@gmail.com>
2016-10-08 20:56:10 -0400

    Initial commit for REST Web Services

---
+++ messenger/src/main/java/com/rsr/messenger/database/DatabaseClass.java
@@ -0,0 +1,26 @@
+package com.rsr.messenger.database;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.rsr.messenger.model.Message;
+import com.rsr.messenger.model.Profile;
+
+public class DatabaseClass {
+
+	private static Map<Long, Message> messages = new HashMap<>();
+	private static Map<String, Profile> profiles = new HashMap<>();
+
+	
+	public static Map<Long, Message> getMessages() {
+		return messages;
+	}
+	
+	public static Map<String, Profile> getProfiles() {
+		return profiles;
+	}
+
+	
+	
+	
+}
---
+++ messenger/src/main/java/com/rsr/messenger/exception/DataNotFoundException.java
@@ -0,0 +1,15 @@
+package com.rsr.messenger.exception;
+
+public class DataNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6328286661536343936L;
+
+	
+	public DataNotFoundException(String message) {
+		super(message);
+	}
+	
+}
