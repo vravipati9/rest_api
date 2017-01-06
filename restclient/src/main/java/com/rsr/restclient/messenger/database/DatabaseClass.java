8cc8fe0598b2a32d7fb85c09c8623eed780ef05d
vvsrk <vvsrk.siva@gmail.com>
2017-01-06 18:37:42 -0500

    added to test branching

--- restclient/src/main/java/com/rsr/restclient/messenger/database/DatabaseClass.java
+++ restclient/src/main/java/com/rsr/restclient/messenger/database/DatabaseClass.java
@@ -11,17 +11,11 @@ public class DatabaseClass {
 	private static Map<Long, Message> messages = new HashMap<>();
 	private static Map<String, Profile> profiles = new HashMap<>();
 
-	
 	public static Map<Long, Message> getMessages() {
 		return messages;
 	}
 	
-	
 	public static Map<String, Profile> getProfiles() {
 		return profiles;
 	}
-
-	
-	
-	
 }
